a2c84bb54975b6979fc5e199b7fdc0b76c3f57a8
byllgrim <robin.a.t.pedersen@gmail.com>
2017-01-25 09:20:22 +0100

    plan functions and macros to implement

---
+++ ansicurses.c
@@ -0,0 +1,21 @@
+/* implement from curses.h
+ * 	stdscr
+ * 	COLS
+ * 	LINES
+ * 	TABSIZE
+ * 	initscr
+ * 	raw
+ * 	noecho
+ * 	endwin
+ * 	getch
+ * 	printw
+ * 	move
+ * 	getcury
+ * 	refresh
+ * 	mvprintw
+ *
+ * additionally from libncursesw.a
+ *	wmove
+ *	wrefresh
+ *	wgetch
+ */
