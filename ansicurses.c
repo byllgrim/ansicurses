516698dcad74b12501c658de4fbb3884760ad069
byllgrim <robin.a.t.pedersen@gmail.com>
2017-01-25 11:04:08 +0100

    implement initscr()

--- ansicurses.c
+++ ansicurses.c
@@ -1,15 +1,15 @@
-/* implement from curses.h
+/* TODO from curses.h
  * 	stdscr
  * 	COLS
  * 	LINES
  * 	TABSIZE
- * 	initscr
  * 	raw
  * 	noecho
  * 	endwin
  * 	getch
  * 	printw
  * 	move
+ * 	getcurx
  * 	getcury
  * 	refresh
  * 	mvprintw
@@ -19,3 +19,37 @@
  *	wrefresh
  *	wgetch
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include "ansicurses.h"
+
+static struct termios saved_attr;
+
+int
+getch(void)
+{
+	return 0; /* TODO */
+}
+
+WINDOW *
+initscr(void)
+{
+	struct termios attr;
+
+	if (!isatty(STDIN_FILENO)) {
+		fprintf(stderr, "not a terminal\n");
+		exit(EXIT_FAILURE);
+		/* TODO die() */
+	}
+
+	tcgetattr(STDIN_FILENO, &saved_attr);
+	/* TODO? atexit(reset_attr); */
+
+	tcgetattr(STDIN_FILENO, &attr);
+	attr.c_lflag &= ~ECHO;
+	tcsetattr (STDIN_FILENO, TCSAFLUSH, &attr);
+
+	return NULL;
+}
--- ansicurses.h
+++ ansicurses.h
@@ -1,3 +1,8 @@
 /* License is unlicense.
  * There are no ifdef guards, because I oppose nested includes.
  */
+
+#define WINDOW void
+
+int getch(void);
+WINDOW *initscr(void);
